8afd28d4f372779432544ab96158c820e826a66e
DevonBrodka <djbrodka@gmail.com>
2026-04-16 02:23:16 -0400

    pull more comments

--- Aura.Core/Interfaces/IAuraGraphRepository.cs
+++ Aura.Core/Interfaces/IAuraGraphRepository.cs
@@ -6,24 +6,19 @@ using Aura.Core.Entities;
 
 public interface IAuraGraphRepository
 {
-    // Node Operations
     ThoughtNode GetNode(Guid id);
     IEnumerable<ThoughtNode> GetAllNodes();
     void AddNode(ThoughtNode node);
     void UpdateNode(ThoughtNode node);
     void RemoveNode(Guid id);
 
-    // Connection / Edge Operations
     void ConnectNodes(Guid sourceId, Guid targetId, float strength, string description = "");
     void DisconnectNodes(Guid sourceId, Guid targetId);
     
-    // Advanced Graph & Spatial Queries
     IEnumerable<ThoughtNode> GetNeighbors(Guid nodeId, int maxDepth = 1);
     
-    // Finding nodes near a coordinate in your VR space
     IEnumerable<ThoughtNode> GetNodesInRadius(AuraVector3 centerPoint, float radius);
 
-    // Constellation Operations
     void AddConstellation(Constellation constellation);
     void MapNodeToConstellation(Guid nodeId, Guid constellationId);
     IEnumerable<Constellation> GetAllConstellations();
